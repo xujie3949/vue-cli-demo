6b5cba54c671baa43e7d0ed48d1723c8be7adb46
徐杰 <xujie@aerozhonghuan.com>
2019-10-15 18:10:43 +0800

    修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    env: {
+        es6: true,
+        node: true,
+        browser: true
+    },
+    extends: ['plugin:vue/recommended', '@vue/prettier'],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-unused-vars': [
+            'error',
+            {
+                vars: 'all',
+                args: 'none'
+            }
+        ]
+    }
 };
