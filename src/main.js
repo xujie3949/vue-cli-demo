222d07c8751bcd299eefac31334687d405f55e24
徐杰 <xujie@aerozhonghuan.com>
2019-10-24 20:07:21 +0800

    升级到vue-cli4

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Element from 'element-ui';
+import Components from '@/components';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+
+import '@/icons';
 
 Vue.config.productionTip = false;
+
+Vue.use(Element);
+Vue.use(Components);
 
 new Vue({
     router,
     store,
     render: h => h(App)
 }).$mount('#app');
+
+// console.log('aaaa');
